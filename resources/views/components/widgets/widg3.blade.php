3bcd2c9742cd7acf5a6b5490adfc2990d857c4a2
Jerry <azhagirishankar@appstronauts.in>
2025-01-10 09:56:51 +0530

    #first commit

---
+++ resources/views/components/widgets/widg3.blade.php
@@ -0,0 +1,32 @@
+<div class="xxl:col-span-3 xl:col-span-6 col-span-12">
+
+    <div class="box overflow-hidden main-content-card">
+        <div class="box-body">
+            <div class="flex items-center gap-2 justify-between flex-wrap">
+                <div class="flex items-start gap-2">
+                    <div
+                        class="leading-none avatar avatar-sm p-1 bg-light avatar-rounded">
+                        <img
+                            src="{{asset('build/assets/images/crypto-currencies/crypto-icons/binance-usd-busd-logo.svg')}}"
+                            alt="" class="w-auto">
+                    </div>
+                    <div>
+                        <h5 class="mb-0">578.05 <span
+                                class="text-textmuted dark:text-textmuted/50 font-medium text-[13px]">
+                                                                    BNB</span></h5>
+                        <p class="mb-0 text-textmuted dark:text-textmuted/50 text-xs">
+                            Binanace</p>
+                    </div>
+                </div>
+                <div id="bnb-marketcap"></div>
+            </div>
+            <div
+                class="flex items-center gap-2 justify-between mt-3 bg-light px-2 p-1 rounded-md">
+                <p class="mb-0 font-medium text-textmuted dark:text-textmuted/50 py-1">
+                    $26,478.09 USD</p>
+                <div class="text-danger"><i class="ti ti-trending-down me-1"></i>-0.12%
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
